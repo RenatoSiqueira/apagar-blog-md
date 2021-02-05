047b24ac3cb7c1d4c509963e1083aa690b682cf8
renatosiqueira <renatoelysiqueira@gmail.com>
2021-02-05 00:51:41 -0300

    changes

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,27 +1,27 @@
 module.exports = {
-  plugins:[
-    'gatsby-plugin-sharp',
-    'gatsby-transformer-sharp',
- {
-      resolve: 'gatsby-source-filesystem',
+  plugins: [
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
       options: {
-        path: __dirname + '/src/content',
-        name: 'pages'
-      }
+        path: __dirname + "/src/content",
+        name: "pages",
+      },
     },
     {
-    resolve: 'gatsby-transformer-remark',
-    options: {
-      plugins:[
-        {
-          resolve: 'gatsby-remark-images',
-          options: {
-            maxWidth: 540
-          }
-      }
-    ]
-    }
+      resolve: "gatsby-transformer-remark",
+      options: {
+        plugins: [
+          {
+            resolve: "gatsby-remark-images",
+            options: {
+              maxWidth: 540,
+            },
+          },
+        ],
+      },
     },
-    'gatsby-plugin-netlify-cms'
-  ]
-}
+    "gatsby-plugin-netlify-cms",
+  ],
+};
